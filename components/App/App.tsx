c485b48627d924daff879778a54057abd308ad49
Vincent Escoffier <vincent.escoffier1@gmail.com>
2022-10-10 10:37:31 +0200

    draw tree at scale

--- components/App/App.tsx
+++ components/App/App.tsx
@@ -10,6 +10,7 @@ import {
 } from "react";
 import rough from "roughjs/bin/rough";
 import { RoughCanvas } from "roughjs/bin/canvas";
+import { Point } from "roughjs/bin/geometry";
 
 type Tree = Category[];
 
@@ -82,13 +83,23 @@ function guidGenerator() {
   );
 }
 
+function getTransformedPoint(
+  x: number,
+  y: number,
+  context: CanvasRenderingContext2D
+) {
+  const originalPoint = new DOMPoint(x, y);
+  return context.getTransform().invertSelf().transformPoint(originalPoint);
+}
+
 function drawBubble(
   canvas: HTMLCanvasElement,
   x: number,
   y: number,
   w: number,
   h: number,
-  radius: number
+  radius: number,
+  rc: RoughCanvas
 ) {
   const ctx = canvas.getContext("2d")!;
   const r = x + w;
@@ -96,6 +107,7 @@ function drawBubble(
   ctx.beginPath();
   ctx.strokeStyle = "black";
   ctx.lineWidth = 2;
+  ctx.textAlign = "center";
   ctx.moveTo(x + radius, y);
   ctx.lineTo(x + radius / 2, y - 10);
   ctx.lineTo(x + radius * 2, y);
@@ -107,7 +119,13 @@ function drawBubble(
   ctx.quadraticCurveTo(x, b, x, b - radius);
   ctx.lineTo(x, y + radius);
   ctx.quadraticCurveTo(x, y, x + radius, y);
-  ctx.fillText("hello world", x + 20, y + h / 2);
+  ctx.fillStyle = colors[2];
+  ctx.fill();
+  ctx.font = `${FONT_SIZE}px Comic Sans MS`;
+  ctx.fillStyle = "black";
+
+  ctx.fillText("hello world", x + w / 2, y + h / 2);
+
   ctx.stroke();
 }
 
@@ -195,24 +213,6 @@ const tree: Tree = [
   },
 ];
 
-export const zoomIn = (
-  <svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 448 512">
-    <path
-      fill="currentColor"
-      d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
-    />
-  </svg>
-);
-
-export const zoomOut = (
-  <svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 448 512">
-    <path
-      fill="currentColor"
-      d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
-    />
-  </svg>
-);
-
 function isArrowKey(keyCode: string) {
   return (
     keyCode === KEYS.ARROW_LEFT ||
@@ -220,6 +220,17 @@ function isArrowKey(keyCode: string) {
     keyCode === KEYS.ARROW_DOWN ||
     keyCode === KEYS.ARROW_UP
   );
+}
+
+function toCanvasCoord(
+  x: number,
+  y: number,
+  context: CanvasRenderingContext2D
+) {
+  const transform = context.getTransform();
+  // Destructure to get the x and y values out of the transformed DOMPoint.
+  const { x: newX, y: newY } = transform.transformPoint(new DOMPoint(x, y));
+  return { x: newX, y: newY };
 }
 
 function hitTest(
@@ -230,13 +241,9 @@ function hitTest(
   cameraZoom: number
 ) {
   const context = canvas.getContext("2d")!;
-  const transform = context.getTransform();
   // Destructure to get the x and y values out of the transformed DOMPoint.
-  const { x: newX, y: newY } = transform.transformPoint(
-    new DOMPoint(element.x, element.y)
-  );
+  const { x: newX, y: newY } = toCanvasCoord(element.x, element.y, context);
   //const {x, y} = getXY(mx, my)
-  context.restore();
   return (
     x > newX &&
     x < newX + RC_WIDTH * cameraZoom &&
@@ -255,22 +262,103 @@ const colors = ["gray", "orange", "#82c91e"];
 
 function drawLeaf(
   rc: RoughCanvas,
+  ctx: CanvasRenderingContext2D,
+  element: Element
+) {
+  const { x, y, seed, color, text, icon } = element;
+  rc.rectangle(x, y, RC_WIDTH, RC_HEIGTH, {
+    fill: color,
+    fillWeight: 0.5, // thicker lines for hachure,
+    seed,
+    fillStyle: "solid",
+  });
+  ctx.font = `${FONT_SIZE}px Comic Sans MS`;
+  ctx.textAlign = "center";
+  ctx.fillText(
+    `x: ${Math.floor(toCanvasCoord(x, y, ctx).x)}, y: ${Math.floor(
+      toCanvasCoord(x, y, ctx).y
+    )}`,
+    x + RC_WIDTH / 2,
+    y + RC_HEIGTH / 2
+  );
+  ctx.font = `${FONT_SIZE + 4}px Comic Sans MS`;
+  ctx.fillText(icon, x + RC_WIDTH / 2, y + RC_HEIGTH / 2 + RC_HEIGTH / 3);
+}
+
+function drawRectAtScale(
+  ctx: CanvasRenderingContext2D,
+  scale: number,
+  width: number,
+  height: number,
+  color: string
+) {
+  ctx.save();
+  ctx.translate(width / 2, height / 2);
+  ctx.scale(scale, scale);
+  ctx.fillStyle = color;
+  ctx.fillRect(-20, -10, 40, 20);
+  ctx.restore();
+}
+
+function drawTreeAtScale(
+  ctx: CanvasRenderingContext2D,
+  scale: number,
+  width: number,
+  height: number,
+  color: string,
+  rc: RoughCanvas
+) {
+  ctx.save();
+  ctx.translate(width / 2, height / 2);
+  ctx.scale(scale, scale);
+  ctx.translate(-120, -200);
+  rc.path(  
+    "m242.559,115.437c-2.403,-2.589 -4.979,-4.97 -7.688,-7.16c1.357,-3.439 2.111,-7.179 2.111,-11.094c0,-16.718 -13.602,-30.32 -30.32,-30.32c-0.985,0 -1.958,0.051 -2.92,0.143c-3.057,-37.455 -34.495,-67.006 -72.724,-67.006c-25.82,0 -48.535,13.489 -61.514,33.778c-2.793,-0.471 -5.657,-0.729 -8.582,-0.729c-28.38,0 -51.469,23.089 -51.469,51.469c0,11.51 3.798,22.148 10.206,30.73c-0.06,0.064 -0.123,0.124 -0.182,0.189c-12.56,13.529 -19.477,31.152 -19.477,49.624c0,40.247 32.743,72.989 72.99,72.989c3.792,0 7.531,-0.292 11.195,-0.85l26.937,41.618l-13.127,85.065c-0.419,2.728 0.295,5.331 2.011,7.331c1.732,2.019 4.347,3.177 7.172,3.177l48.027,0c2.825,0 5.438,-1.158 7.171,-3.176c1.716,-2 2.43,-4.604 2.01,-7.334l-12.905,-83.635l27.736,-42.852c3.226,0.43 6.506,0.656 9.828,0.656c40.247,0 72.99,-32.743 72.99,-72.989c0,-18.472 -6.917,-36.096 -19.476,-49.624zm-110.795,140.059l-18.911,-29.218c6.892,-4.498 13.043,-10.193 18.165,-16.928c5.382,7.077 11.902,13.001 19.223,17.6l-18.477,28.546z",
+    {
+      seed: 2,
+      fill: color,
+    }
+  );
+  ctx.restore();
+}
+
+function drawTreeSvg(rc: RoughCanvas, ctx: CanvasRenderingContext2D) {
+  const width = ctx.canvas.width;
+  const height = ctx.canvas.height;
+  drawTreeAtScale(ctx, 1, width, height, colors[0], rc);
+  drawTreeAtScale(ctx, 3, width, height, colors[2], rc);
+
+}
+
+function drawIt(
+  rc: RoughCanvas,
   canvas: HTMLCanvasElement,
   elements: Element[]
 ) {
   const ctx = canvas.getContext("2d")!;
-  for (const { x, y, seed, color, text, icon } of elements) {
-    rc.rectangle(x, y, RC_WIDTH, RC_HEIGTH, {
-      fill: color,
-      fillWeight: 0.5, // thicker lines for hachure,
-      seed,
-    });
-    ctx.font = `${FONT_SIZE}px Comic Sans MS`;
-    ctx.textAlign = "center";
-    ctx.fillText(text, x + RC_WIDTH / 2, y + RC_HEIGTH / 2);
-    ctx.font = `${FONT_SIZE + 4}px Comic Sans MS`;
-    ctx.fillText(icon, x + RC_WIDTH / 2, y + RC_HEIGTH / 2 + RC_HEIGTH / 3);
+  drawTreeSvg(rc, ctx);
+  for (const element of elements) {
+    drawLeaf(rc, ctx, element);
   }
+  let points: Point[] = [];
+  for (let i = 0; i < 20; i++) {
+    let x = (400 / 20) * i + 10;
+    let xdeg = (Math.PI / 100) * x;
+    let y = Math.round(Math.sin(xdeg) * 90) + 500;
+    points.push([x, y]);
+  }
+  rc.curve(points, {
+    stroke: "red",
+    strokeWidth: 3,
+    seed: 4,
+  });
+  const p = new Path2D(
+    "M15 3 Q16.5 6.8 25 18 A12.8 12.8 0 1 1 5 18 Q13.5 6.8 15 3z"
+  );
+  ctx.fillStyle = colors[2];
+  ctx.fill(p);
+  ctx.stroke(p);
+  //drawBubble(canvas, 10, 60, 220, 90, 20, rc);
   /*
   rc.path("M298.736,115.437c-2.403-2.589-4.979-4.97-7.688-7.16c1.357-3.439,2.111-7.179,2.111-11.094  c0-16.718-13.602-30.32-30.32-30.32c-0.985,0-1.958,0.051-2.92,0.143C256.862,29.551,225.424,0,187.195,0  c-25.82,0-48.535,13.489-61.514,33.778c-2.793-0.471-5.657-0.729-8.582-0.729c-28.38,0-51.469,23.089-51.469,51.469  c0,11.51,3.798,22.148,10.206,30.73c-0.06,0.064-0.123,0.124-0.182,0.189c-12.56,13.529-19.477,31.152-19.477,49.624  c0,40.247,32.743,72.989,72.99,72.989c3.792,0,7.531-0.292,11.195-0.85l26.937,41.618l-13.127,85.065  c-0.419,2.728,0.295,5.331,2.011,7.331c1.732,2.019,4.347,3.177,7.172,3.177h48.027c2.825,0,5.438-1.158,7.171-3.176  c1.716-2,2.43-4.604,2.01-7.334l-12.905-83.635l27.736-42.852c3.226,0.43,6.506,0.656,9.828,0.656  c40.247,0,72.99-32.743,72.99-72.989C318.212,146.589,311.295,128.965,298.736,115.437z M187.941,255.496l-18.911-29.218  c6.892-4.498,13.043-10.193,18.165-16.928c5.382,7.077,11.902,13.001,19.223,17.6L187.941,255.496z"
   , {
@@ -284,7 +372,7 @@ function drawLeaf(
   rc.path(
     "M468.607,0c-12.5,1-304.4,8.3-395,99c-92.8,93.8-98,240.8-10.4,328.3c92.8,85.9,227.6,85.3,327.3-11.5   c90.7-89.6,99-382.5,99-395C487.807,8,480.707,1.3,468.607,0z M362.307,387.8c-83.3,79.7-190.7,72.7-254.4,24l79.2-79.2l148.1-15.7   c10.4-1,18.8-10.4,17.7-21.9c-1-10.4-10.4-18.8-21.9-17.7l-98.7,10.1l42.4-42.4c8.3-8.3,8.3-20.8,0-29.2c-8.3-8.3-20.8-8.3-29.2,0   l-43.7,43.7l10.3-101c2.1-10.4-6.3-20.8-17.7-21.9c-10.4-2.1-20.8,6.3-21.9,17.7l-15.9,150.4l-78,78   c-57.2-73-47.7-183.7,24.1-255.5c60.5-59.4,253.3-81.3,346.1-85.5C444.607,134.5,422.707,327.3,362.307,387.8z",
     { fill: "rgb(10,150,10)", seed: 2 }
-  );
+  );  
   ctx.font = "20px Comic Sans MS";
   ctx.fillStyle = "white";
   ctx.textAlign = "center";
@@ -347,7 +435,7 @@ function draw(
     ctx
   );
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawLeaf(roughCanvas, canvas, elements);
+  drawIt(roughCanvas, canvas, elements);
   //buildTree(ctx, roughCanvas);
 }
 
@@ -409,6 +497,24 @@ function buildTree(
   drawTree(x1, y1, x2, y2, angle, depth, context, roughCanvas);
 }
 
+function mousePosToCanvasPos(
+  context: CanvasRenderingContext2D,
+  x: number,
+  y: number
+) {
+  const transform = context.getTransform();
+  var matrix = context.getTransform(); // W3C (future)
+  var imatrix = matrix.invertSelf(); // invert
+
+  // apply to point:
+  const px = x * imatrix.a + y * imatrix.c + imatrix.e;
+  const py = x * imatrix.b + y * imatrix.d + imatrix.f;
+  return {
+    x: px,
+    y: py,
+  };
+}
+
 function getMousePos(canvas: HTMLCanvasElement, evt: any) {
   var rect = canvas.getBoundingClientRect();
   return {
@@ -420,13 +526,14 @@ let MAX_ZOOM = 5;
 let MIN_ZOOM = 0.1;
 let SCROLL_SENSITIVITY = 0.0005;
 let STEP = 50;
+let INITIAL_ZOOM = 0.5;
 export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [width, height, devicePixelRatio] = useDeviceSize();
   const posRef = useRef<HTMLDivElement>(null);
   const [roughCanvas, setRoughCanvas] = useState<RoughCanvas | null>(null);
   const [appState, setAppState] = useState<AppState>(() => ({
-    cameraZoom: 1,
+    cameraZoom: INITIAL_ZOOM,
     scaleMultiplier: 0.8,
     cameraOffset: { x: width / 2, y: height / 2 },
     isDragging: false,
@@ -436,17 +543,17 @@ export default function Canvas() {
     mode: "select",
     elements: [
       {
-        x: 300,
-        y: 300,
+        x: 0,
+        y: 0,
         seed: getRandomArbitrary(1, 1000),
-        color: "rgb(10,150,10)",
+        color: colors[2],
         id: guidGenerator(),
         text: "go muscu",
         icon: "💪",
       },
       {
-        x: 400,
-        y: 500,
+        x: window.innerWidth,
+        y: window.innerHeight,
         seed: getRandomArbitrary(1, 1000),
         color: "gray",
         id: guidGenerator(),
@@ -477,7 +584,6 @@ export default function Canvas() {
 
   useEffect(() => {
     function handleKeyDown(e: KeyboardEvent) {
-      console.log(e.code);
       if (isArrowKey(e.code)) {
         if (e.key === KEYS.ARROW_LEFT)
           setAppState((prev) => ({
@@ -599,17 +705,12 @@ export default function Canvas() {
 
   const handlePointerMove = (e: PointerEvent<HTMLCanvasElement>) => {
     const { x, y } = getMousePos(canvasRef.current!, e);
+
     if (appState.draggedElement) {
       const canvas = canvasRef.current!;
-      let { x: startX, y: startY } = appState.draggedElement;
       const context = canvas.getContext("2d")!;
-      const transform = context.getTransform();
-      var matrix = context.getTransform(); // W3C (future)
-      var imatrix = matrix.invertSelf(); // invert
-
-      // apply to point:
-      const px = x * imatrix.a + y * imatrix.c + imatrix.e;
-      const py = x * imatrix.b + y * imatrix.d + imatrix.f;
+      let { x: startX, y: startY } = appState.draggedElement;
+      const { x: px, y: py } = mousePosToCanvasPos(context, x, y);
       const dx = px - startX;
       const dy = py - startY;
       const dragTarget = {
@@ -671,14 +772,12 @@ export default function Canvas() {
       if (zoomAmount) {
         cameraZoom += zoomAmount;
       } else if (zoomFactor) {
-        console.log(zoomFactor);
         cameraZoom = zoomFactor * lastZoom.current;
       }
 
       cameraZoom = Math.min(cameraZoom, MAX_ZOOM);
       cameraZoom = Math.max(cameraZoom, MIN_ZOOM);
       setAppState((prev) => ({ ...prev, cameraZoom }));
-      console.log(zoomAmount);
     }
   }
 
@@ -696,6 +795,7 @@ export default function Canvas() {
         onClick={(e) => {
           if (appState.mode !== "select") return;
           const { x, y } = getEventLocation(e);
+          console.log({ x, y });
           for (const element of elements) {
             if (hitTest(x, y, element, canvasRef.current!, cameraZoom)) {
               setAppState((prev) => ({
@@ -759,13 +859,24 @@ export default function Canvas() {
         >
           Add
         </button>
-        <button onClick={(e) => adjustZoom(0.25, null)}>+</button>
-        <button onClick={(e) => adjustZoom(-0.25, null)}>-</button>
         Mode:
         <select value={appState.mode} onChange={(e) => setMode(e.target.value)}>
           <option value="drag">drag</option>
           <option value="select">select</option>
         </select>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          display: "flex",
+          gap: 10,
+          bottom: 10,
+          left: 10,
+        }}
+      >
+        <button onClick={(e) => adjustZoom(-0.25, null)}>-</button>
+        <div>{Math.floor(cameraZoom * 100)}%</div>
+        <button onClick={(e) => adjustZoom(0.25, null)}>+</button>
       </div>
     </div>
   );
