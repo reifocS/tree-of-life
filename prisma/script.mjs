01230f6c2992ac102ae0c2fcab5a33b7167c591b
RaphaelPainter <raphael.painter@imt-atlantique.net>
2022-11-02 08:38:38 +0100

    feat(db) findalluser

--- prisma/script.mjs
+++ prisma/script.mjs
@@ -5,8 +5,9 @@ const prisma = new PrismaClient()
 async function main() {
   await myReset(0);
   await myPopulate(1);
-  await myDisplay(2);
-  await myReset(3);
+  await myUsingAPI(2);
+  await myDisplay(3);
+  await myReset(4);
 }
 
 async function myPopulate(step) {
@@ -28,6 +29,34 @@ async function myPopulate(step) {
       branchId: branch.id
     },
   })
+  await prisma.role.create({
+    data: {
+      label: 'admin',
+      id: 3
+    },
+  })
+  await prisma.role.create({
+    data: {
+      label: 'soignant',
+      id: 2
+    },
+  })
+  await prisma.role.create({
+    data: {
+      label: 'patient',
+      id: 1
+    },
+  })
+}
+
+async function myUsingAPI(step) {
+  console.log(step + ") using api");
+  let user = await createUser("Géraud", 1);
+  user = updateUser(user.id, "superGéraud", 3);
+  console.log(await findAllUser());
+  console.log(await findUserByID(user.id));
+  console.log(await deleteUser(user.id));
+
 }
 
 async function myDisplay(step) {
@@ -38,6 +67,10 @@ async function myDisplay(step) {
   console.log(branches);
   const leaves = await prisma.leaf.findMany();
   console.log(leaves);
+  const roles = await prisma.role.findMany();
+  console.log(roles);
+  const user = await prisma.user.findMany();
+  console.log(user);
 }
 
 async function myReset(step) {
@@ -45,9 +78,9 @@ async function myReset(step) {
   await prisma.leaf.deleteMany();
   await prisma.branch.deleteMany();
   await prisma.tree.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.role.deleteMany();
 }
-
-
 
 main()
   .then(async () => {
@@ -59,3 +92,44 @@ main()
     process.exit(1)
   })
 
+//USER
+async function findAllUser() {
+  return prisma.user.findMany();
+}
+async function findUserByID(idp) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: idp,
+    },
+  })
+  return user;
+}
+async function deleteUser(idp) {
+  const user = await prisma.user.delete({
+  where: {
+    id:idp,
+  },
+  })
+  return user;
+}
+async function createUser(nomp, roleIDp) {
+  const user = await prisma.user.create({
+  data: {
+    nom: nomp,
+    roleID: roleIDp
+  },
+  })
+  return user;
+}
+async function updateUser(idp, nomp, roleIDp) {
+  const user = await prisma.user.update({
+  where: {
+    id: idp,
+  },
+  data: {
+    nom: nomp,
+    roleID: roleIDp,
+  },
+  })
+  console.log(user);
+}
