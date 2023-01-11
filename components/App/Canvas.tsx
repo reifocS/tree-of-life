5bcc9099c9a0059c49290a1c476c55817b3fc0d9
Vincent Escoffier <vincent.escoffier1@gmail.com>
2023-01-11 12:10:55 +0100

    remove keyboard from canvas edition view

--- components/App/Canvas.tsx
+++ components/App/Canvas.tsx
@@ -105,7 +105,7 @@ export default function Canvas({ treeFromModel }: { treeFromModel: Model }) {
     }
   }
 
-  useKeyboard(setAppState);
+  //useKeyboard(setAppState);
   const {
     cameraZoom,
     elements,
