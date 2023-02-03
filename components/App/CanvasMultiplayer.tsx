387486a53285043bf673e79b613fe0752083de25
Vincent Escoffier <vincent.escoffier1@gmail.com>
2023-02-03 10:08:09 +0100

    feat: :sparkles: back link in canvas multiplayer

--- components/App/CanvasMultiplayer.tsx
+++ components/App/CanvasMultiplayer.tsx
@@ -32,6 +32,7 @@ import Editor from "../Editor/Editor";
 import { useRouter } from "next/router";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import { Seance } from "../../types";
+import Link from "next/link";
 
 export default function Canvas({
   isOwner,
@@ -252,6 +253,22 @@ export default function Canvas({
   const canvas = canvasRef.current;
   return (
     <>
+      {isOwner && (
+        <Link className="absolute left-[30px] top-[30px]" href="/patients">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="black"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Link>
+      )}
       <div className="container">
         {/*<Legend />*/}
         {others?.map(({ presence, connectionId }) => {
