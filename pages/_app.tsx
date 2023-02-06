df65981a3be86bff7860008ccb307d2507361bb7
Vincent Escoffier <vincent.escoffier1@gmail.com>
2023-02-06 16:58:41 +0100

    versionning to clear local storage on breaking code update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from '@next/font/google'
+import { useEffect } from "react";
 const inter = Inter({ subsets: ['latin'] })
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const version = "1.0.5"; // remember to update this
+    if (localStorage.getItem("version") != version) {
+      localStorage.clear();
+      localStorage.setItem("version", version);
+    }
+  });
   return (
     <>
       <style jsx global>{`
